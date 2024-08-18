203b8c04571c2c7589eb5287c55df4f5add5fe1c
gaurav-agarwal-code <986gauravagarwal@gmail.com>
2024-08-18 11:42:16 +0530

    uploaded update facility

--- imageshare/src/App.jsx
+++ imageshare/src/App.jsx
@@ -1,23 +1,48 @@
-import { useState } from 'react'
-import {BrowserRouter, Routes , Route} from "react-router-dom"
-import { Navbar } from './component/Navbar/Navbar.jsx'
-import {Home} from "./pages/Home.jsx"
-import {Share} from "./pages/Share.jsx"
-import {Register} from "./pages/Register.jsx"
-import {Login} from "./pages/Login.jsx"
-import { About } from './pages/About.jsx'
-import { Footer } from './component/Footer/Footer.jsx'
-import { Error } from './pages/Error.jsx'
-import {Logout} from './pages/Logout.jsx'
-import { Contact } from './pages/Contact.jsx'
-import { Upload } from './pages/Upload.jsx'
-import { UploadForm } from './component/UploadForm.jsx'
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar } from './component/Navbar/Navbar.jsx';
+import { Home } from "./pages/Home.jsx";
+import { Share } from "./pages/Share.jsx";
+import { Register } from "./pages/Register.jsx";
+import { Login } from "./pages/Login.jsx";
+import { About } from './pages/About.jsx';
+import { Footer } from './component/Footer/Footer.jsx';
+import { Error } from './pages/Error.jsx';
+import { Logout } from './pages/Logout.jsx';
+import { Contact } from './pages/Contact.jsx';
+import { Upload } from './pages/Upload.jsx';
+import { UploadForm } from './pages/UploadForm.jsx';
+import { useAuth } from './store/auth.jsx';
+import axios from 'axios';
+
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    // if (!user) return;
+
+    const getPhotos = async () => {
+      try {
+        const response = await axios.get("/uploadform/get", {
+          withCredentials: true,
+        });
+        
+        console.log(response.data);
+        setPhotos(response.data);
+      } catch (error) {
+        console.error("Error fetching photos:", error);
+      }
+    };
+
+    getPhotos();
+  }, [user]);
+
 
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -27,12 +52,12 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/uploadform" element={<UploadForm />} />
+        <Route path="/uploadform" element={<UploadForm photos={photos} />} />
         <Route path="*" element={<Error />} />
       </Routes>
-    <Footer />
-    </ BrowserRouter>
-  )
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
